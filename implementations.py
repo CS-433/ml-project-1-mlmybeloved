86252399bbc6543082ee36de3ec92bd9d788208d
Thibaud <thibaud.banderet@gmail.com>
2022-10-06 13:08:16 +0200

    add helpers and copy more from exercises

--- implementations.py
+++ implementations.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""ML functions to be used in the training"""
+from helpers import *
+import numpy as np
 
 def compute_mse(y, tx, w)
     """Calculate the mse loss
@@ -15,15 +18,62 @@ def compute_mse(y, tx, w)
     N = np.size(y)
     return (1/(2*N))*np.sum((y-tx@w)**2)
 
+def compute_mse_gradient(y, tx, w):
+    """Computes the gradient at w.
+        
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,D)
+        w: numpy array of shape=(D, ). The vector of model parameters.
+        
+    Returns:
+        An numpy array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
+    """
+
+    return -1/np.size(y)*tx.T@(y-(tx@w))
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm using MSE.
+        
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+        
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar."""
     
-    
-    return (w, loss)
+    w = initial_w
+    for n_iter in range(max_iters):
+        grad = compute_mse_gradient(y, tx, w)
+        w = w - gamma*grad
+    mse = compute_mse(y, tx, w)
+    return (w, mse)
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD) using MSE (batch size 1).
+            
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(D,). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+        
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar."""
     
-    
-    return (w, loss)
+    w = initial_w
+    for n_iter in range(max_iters):
+        minibatch_y, minibatch_tx = next(batch_iter(y, tx, 1))
+        grad = compute_mse_gradient(minibatch_y, minibatch_tx, w)
+        w = w - gamma*grad
+    mse = compute_mse(y, tx, w)
+    return (w, mse)
 
 def least_squares(y, tx):
     """Calculate the least squares solution.
