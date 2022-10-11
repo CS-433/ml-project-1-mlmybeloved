8931c29424a30d5afc3ef244e97447d34951fd62
Luca Delabarre <luca.delabarre@epfl.ch>
2022-10-11 16:12:57 +0200

    Fixed the code to replace invalid values

--- implementations.py
+++ implementations.py
@@ -4,41 +4,58 @@ from helpers import *
 import numpy as np
 
 def standardize(x):
-    """Standardizes matrix x
+    """Standardizes matrix x.
     
     Args:
         x: numpy array of shape (N,D), D is the number of features.
         
     Returns:
-        x: standardized x matrix
-        mean_x: array of column means of x
-        std_x: array of column standard deviations of x
+        x: standardized x matrix.
+        mean_x: array of column means of x.
+        std_x: array of column standard deviations of x.
     """
     
     mean_x = np.mean(x, axis=0)
     x = x - mean_x
     std_x = np.std(x, axis=0)
-    if std_x: # Avoids NaN columns i.e column is useless
-        x = x / std_x
+    x = x / std_x
     return x, mean_x, std_x
 
 def add_x_bias(x):
-    """Adds bias term in x
+    """Adds bias term in x.
     
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         x: numpy array of shape (N,D), D is the number of features.
         
     Returns:
-        tx: numpy array of shape (N,D+1), created by adding a column of 1s to x
+        tx: numpy array of shape (N,D+1), created by adding a column of 1s to x.
     """
     
     N = x.shape[0]
     tx = np.c_[np.ones(N), x]
     return tx
 
+def replace_min_999_by_col_mean(x):
+    """Replace invalid values -999 by column(feature) average.
+    
+    Args:
+        x: numpy array of shape (N,D), D is the number of features.
+        
+    Returns:
+        x with values -999 replaced by the mean of the column they are in. (mean computed excluding all -999)
+    """
+    
+    mask_999 = np.where(x == -999, 1, 0) # 1 where -999 are, 0 otherwise
+    for i in range(x.shape[1]):
+        col = x[:, i] # Get column
+        mask_col = mask_999[:, i] # Get corresponding mask column
+        col_mean = np.ma.masked_array(col, mask_col).mean(axis=0) # Compute mean without the -999
+        x[:, i] = np.where(col == -999, col_mean, col) # Replace -999 by mean or keep column
+    return x
+
 def compute_mse(y, tx, w):
-    """Calculate the mse loss
+    """Calculate the mse loss.
     
     Args:
         y: numpy array of shape (N,), N is the number of samples.
@@ -54,7 +71,7 @@ def compute_mse(y, tx, w):
     return (1/N)*np.sum(error**2)
 
 def compute_mae(y, tx, w):
-    """Calculate the mae loss
+    """Calculate the mae loss.
     
     Args:
         y: numpy array of shape (N,), N is the number of samples.
@@ -73,8 +90,8 @@ def compute_mse_gradient(y, tx, w):
     """Computes the MSE gradient at w.
         
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,D)
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
         w: numpy array of shape=(D, ). The vector of model parameters.
         
     Returns:
@@ -82,15 +99,15 @@ def compute_mse_gradient(y, tx, w):
     """
     
     N = np.size(y)
-    error = y-tx@w
+    error = y-tx@w # Error vector
     return (-1/N)*tx.T@(error)
 
 def compute_mae_gradient(y, tx, w):
     """Computes the MAE gradient at w.
         
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,D)
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
         w: numpy array of shape=(D, ). The vector of model parameters.
         
     Returns:
@@ -98,7 +115,7 @@ def compute_mae_gradient(y, tx, w):
     """
     
     N = np.size(y)
-    error = y-tx@w
+    error = y-tx@w # Error vector
     error = np.where(error>=0, 1, -1) # error := sign(error)
     return -(1/N)*tx.T@error
 
@@ -106,22 +123,22 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """The Gradient Descent (GD) algorithm using MSE.
         
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,D)
-        initial_w: numpy array of shape=(D,). The initial guess (or the initialization) for the model parameters
-        max_iters: a scalar denoting the total number of iterations of GD
-        gamma: a scalar denoting the stepsize
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
+        initial_w: numpy array of shape=(D,). The initial guess (or the initialization) for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of GD.
+        gamma: a scalar denoting the stepsize.
         
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar."""
+        mse: scalar, mean squared error."""
     
     w = initial_w
     for n_iter in range(max_iters):
-        grad = compute_mse_gradient(y, tx, w)
-        w = w - gamma*grad
+        gradient = compute_mse_gradient(y, tx, w)
+        w = w - gamma*gradient
     mse = compute_mse(y, tx, w)
-    return (w, mse)
+    return w, mse
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     """The Stochastic Gradient Descent algorithm (SGD) using MSE (batch size 1).
